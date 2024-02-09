15904087adb1ab2e43dfff0eef448860ce054d24
Chaim Bienenfeld <chaim@bienenfeld.org>
2024-02-09 14:20:41 +0200

    09/02/24

--- functions.java
+++ functions.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 import java.util.Random;
     public class functions {
-        public static int sheela2(int x,int y){
+        public static int fgh(int x,int y){
             int sum = x + y;
             return sum;
             
             
             
         }
-        public static int Sheela3(int x){
+        public static int sumOfThreeNums(int x){
             int sum = (x%10) + (x/10%10)+(x/100);
             return sum;
         }
-        public static int jkl(int r){
-            return r*6+2;
-        }
-        public static double sheela4(int x){
+        public static double amntOfNums(int x){
+            
             return Math.random()*Math.pow(10,x);
 
         }
@@ -23,34 +21,42 @@ import java.util.Random;
         Scanner s = new Scanner(System.in);  
         
     //sheela 2
-            //sum of nums in sheela2
-            //  int z = sheela2(5, 6);
+            //sum of nums in fgh
+            //  int z = fgh(5, 6);
             //  System.out.println("the sum of nums is "+z);
              
     //sheela 3
-            //     int x;
+            //      int x;
        
-            //     System.out.println("What is your num");
-            //     x = s.nextInt();
+            //      System.out.println("What is your num");
+            //      x = s.nextInt();
             
-            //      if(((x<1000) && (x>99)) || ((x>-1000)&&(x<-99))){
-            //      System.out.println(Sheela3(x));    
-            //      }
-            //else{
-            //     System.out.println("-1");
-            // }
-            //     int num;
-            //     do{
-            //     System.out.println("Write a num");
-            //     num = s.nextInt();
-            //     System.out.println(jkl(num));
-            //    }while(num!=-1);{
-            //    }
+            //       if(((x<1000) && (x>99)) || ((x>-1000)&&(x<-99))){
+            //       System.out.println(sumOfThreeNums(x));    
+            //       }
+            // else{
+            //      System.out.println("-1");
+            //  }
+                
 
     //sheela 4
-             System.out.println("what num");
-             int x = s.nextInt();
-             System.out.printf("%.0f",sheela4(x));
+            //int x;
+            //do{
+            //System.out.println("what num");
+            //x = s.nextInt();
+            //System.out.printf("%.0f",amntOfNums(x));
+            //String str = String.valueOf(x);
+            //System.out.println();
+            //int length = str.length() - 1;
+            
+            //if(x>0)
+            //System.out.println(x + " The length of your num is " + str.length());
+            //else
+            //System.out.println(x + " The length of your num is " + length);
+
+            //}while(x!=-1);{
+            //System.out.println("Hope you enjoyed");
+            //}
         }
         
     }
